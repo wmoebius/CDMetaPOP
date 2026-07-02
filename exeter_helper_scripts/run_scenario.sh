762a41a9a95c75e965804984ecea77ba3f1f48c1
Wolfram Moebius <w.moebius@exeter.ac.uk>
2026-07-02 13:45:54 +0100

    new helper script to run scenario, change RunVars.csv for parallel runs

---
+++ exeter_helper_scripts/run_scenario.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Create subfolder name
+subfolder_name="$1"
+
+# Create output subfolder
+mkdir -p "../output/${subfolder_name}/raw"
+
+# Copy ../toyexample into the subfolder
+cp -rp ../toyexample/* "../output/${subfolder_name}/"
+
+# Run the scenario
+uv run ../src/CDMetaPop.py "../output/${subfolder_name}" RunVars.csv raw/
+
